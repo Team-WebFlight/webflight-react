fad7d0d07ed415a1a92bc316db178e9f52abb264
tbywong <tbywong@gmail.com>
2016-03-30 16:59:34 -0700

    prioritizeFiles function, test, and fixtures

---
+++ test/fixtures/filesArray.js
@@ -0,0 +1,8 @@
+module.exports = {
+  notUniq: ['eagle.jpg', 'falcon.jpg', 'owl.jpg', 'eagle.jpg'],
+  uniq: ['eagle.jpg', 'falcon.jpg', 'owl.jpg', 'birds.gif'],
+  absolute: [ '/Users/Baoyee/Codesmith/wf-react/test/fixtures/assets/images/eagle.jpg',
+  '/Users/Baoyee/Codesmith/wf-react/test/fixtures/assets/images/falcon.jpg',
+  '/Users/Baoyee/Codesmith/wf-react/test/fixtures/assets/images/owl.jpg',
+  '/Users/Baoyee/Codesmith/wf-react/test/fixtures/assets/videos/birds.gif' ]
+}
---
+++ test/fixtures/sortedFilesArray.js
@@ -0,0 +1,4 @@
+module.exports = [ '/Users/Baoyee/Codesmith/wf-react/test/fixtures/assets/images/falcon.jpg',
+  '/Users/Baoyee/Codesmith/wf-react/test/fixtures/assets/images/eagle.jpg',
+  '/Users/Baoyee/Codesmith/wf-react/test/fixtures/assets/videos/birds.gif',
+  '/Users/Baoyee/Codesmith/wf-react/test/fixtures/assets/images/owl.jpg' ]
