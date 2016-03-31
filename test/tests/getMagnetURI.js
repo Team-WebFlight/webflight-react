6e57e4529f70d2737dfb013067f39901aa369298
10000highfives <jared.a.fowler@gmail.com>
2016-03-31 11:21:02 -0700

    Renames fn to avoid confusion with create-torrent package. Better describes fn. Adds unit test.

---
+++ test/tests/getMagnetURI.js
@@ -0,0 +1,13 @@
+/* global describe, it */
+'use strict'
+const chai = require('chai')
+const assert = chai.assert
+const createTorrent = require('../../lib/createTorrent')
+const filesSrcArray = require('../fixtures/filesArray').dirname
+
+//describe('createTorrent', () => {
+//  it('should return a string', () => {
+    
+
+process.stdout.write('files src array ', filesSrcArray)
+
