b413309adf84aeeb5898a979a23cb9035bbb6812
tbywong <tbywong@gmail.com>
2016-04-01 11:13:07 -0700

    added stringifyHtml function and tests

--- index.js
+++ index.js
@@ -2,20 +2,21 @@
 
 const fs = require('fs')
 const path = require('path')
-const child_process = require('child_process')
 
 const botGenerator = require('./lib/botGenerator')
 const createFilesArr = require('./lib/createFilesArr')
 const createHtml = require('./lib/createHtml')
 const createTorrent = require('./lib/createTorrent')
 const prioritizeFiles = require('./lib/prioritizeFiles')
-const writeSeedScript = require('./lib/writeSeedScript')
+const stringifyHtml = require('./lib/stringifyHtml')
 
 /**
 * @param {Object} options
 *   assetsPath: Array          (required)
 *   wfPath: String             (optional - defaults to '/wfPath')
 *   seedScript: String         (optional - defaults to 'wf-seed.js')
+*   htmlInput: String          (optional - defaults to 'index.html')
+*   htmlOutput: String         (optional - defaults to 'wf-index.html')
 *   statusBar: Boolean         (optional - defaults to true)
 *
 * @param {string} serverRoot - path to root folder
@@ -35,6 +36,7 @@ function WebFlight (options, serverRoot) {
   }
 
   this.seedScript = options.seedScript || path.join(this.wfPath, 'js/wf-seed.js')  // default
+  this.htmlInput = options.htmlInput || path.join(serverRoot, 'index.html')  // default
   this.htmlOutput = options.htmlOutput || path.join(this.wfPath, 'wf-index.html')  // non-configurable
   this.statusBar = options.statusBar || true // default
 
@@ -43,10 +45,12 @@ function WebFlight (options, serverRoot) {
 }
 
 WebFlight.prototype.init = function () {
+  const stringifiedHtml = stringifyHtml(this.htmlInput)
+
   createFilesArr(this.assetsPath)
   .then(prioritizeFiles.bind(null, this.seedScript))
   .then(createTorrent.bind(null))
-  .then(createHtml.bind(null, this.htmlOutput))
+  .then(createHtml.bind(null, stringifiedHtml, this.htmlOutput))
   .then(botGenerator.bind(null, this.seedScript))
 }
 
---
+++ test/tests/stringifyHtml.js
@@ -0,0 +1,16 @@
+/* globals describe, it */
+
+'use strict'
+const path = require('path')
+const chai = require('chai')
+const expect = chai.expect
+
+const stringifyHtml = require('../../lib/stringifyHtml.js')
+const html = require('../fixtures/opts.js').opts1.htmlInput
+
+describe('stringifyHtml', () => {
+  const stringifiedHtmlTest = stringifyHtml(html)
+  it('should return a string', () => {
+    expect(stringifiedHtmlTest).to.be.a('string')
+  })
+})
